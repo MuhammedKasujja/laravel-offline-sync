ef6f36f53f58a1aa92760d7c7de1b472b9b53164
Muhammed Kasujja <al.kasmud.2@gmail.com>
2023-07-05 09:38:34 +0300

    feat: route for checking for db changes

--- Modules/OfflineOnlineSync/Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('offlineonlinesync::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('offlineonlinesync.name') !!}
-    </p>
-@endsection
